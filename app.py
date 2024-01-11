2ab9ff0ad372570d00276d4b80fe8c95574574be
Dina <lutdina04@yandex.ru>
2024-01-11 17:22:05 +1000

    change main.html change registration.html

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 import re
 import hashlib
 from datetime import datetime
-from flask import Flask, render_template, request, jsonify, url_for, session, redirect
+from flask import Flask, render_template, request, jsonify, url_for, session
 import os
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager, login_required
+
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
@@ -13,10 +13,8 @@ app.config['SECRET_KEY'] = 'your_secret_key'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'identifier.db')
 db = SQLAlchemy(app)
 
-
 def hash_password(password):
     return hashlib.sha256(password.encode('utf-8')).hexdigest()
-
 
 @app.route('/')
 def main():
@@ -26,7 +24,6 @@ def main():
 @app.route('/login', methods=['GET'])
 def login_form():
     return render_template('pages/entrance.html')
-
 
 @app.route('/submit_login', methods=['POST'])
 def submit_login():
@@ -42,7 +39,6 @@ def submit_login():
     else:
         return jsonify({'error': 'Неправильный email или пароль'})
 
-
 @app.route('/registration', methods=['GET'])
 def registration_form():
     return render_template('pages/registration.html')
@@ -56,11 +52,15 @@ def submit_registration():
     first_name = data.get('first_name')
     session['first_name'] = first_name
     last_name = data.get('last_name')
+    session['last_name'] = last_name
     middle_name = data.get('middle_name')
+    session['middle_name'] = middle_name
     city = data.get('city')
+    session['city'] = city
     birthday = datetime.strptime(data.get('birthday'), '%Y-%m-%d').date()
+    session['birthday'] = birthday
     email = data.get('email')
-
+    session['email'] = email
     password = data.get('password')
     hashed_password = hash_password(password)
     if not re.match(r"[^@]+@[^@]+\.[^@]+", email):
@@ -70,8 +70,9 @@ def submit_registration():
     if not re.match(r'^\d{10}$', phone_number):
         return False
 
+
     new_user = Users(first_name=first_name, last_name=last_name, middle_name=middle_name, email=email,
-                     phone_number=phone_number, password=hashed_password)
+                     phone_number=phone_number,  password=hashed_password)
 
     db.session.add(new_user)
     db.session.commit()
@@ -82,17 +83,22 @@ def submit_registration():
 
     return jsonify({'redirect': url_for('show_result_page')})
 
-
 @app.route('/result', methods=['GET'])
 def show_result_page():
     user_name = session.get('first_name')
-    return render_template('pages/result.html', user_name=user_name)
+    user_fam = session.get('last_name')
+    user_ot = session.get('middle_name')
+    user_sity = session.get('city')
+    user_bith = session.get('birthday')
+    user_email = session.get('email')
+
+    return render_template('pages/result.html', user_name=user_name, user_fam=user_fam, user_ot=user_ot,
+                           user_sity=user_sity, user_bith=user_bith, user_email=user_email)
 
 
 @app.route('/main', methods=['GET', 'POST'])
-@login_required
 def main_page():
-    from models import Users, User_details
+    from models import Users
     if request.method == 'POST':
         email = request.form['email']
         user = Users.query.filter_by(email=email).first()
@@ -107,40 +113,13 @@ def main_page():
 
     return render_template('pages/index.html')
 
-
 @app.route('/tests')
 def test():
     return render_template('pages/test.html')
 
-
 @app.route('/news')
 def news():
     return render_template('pages/news.html')
-
-
-@app.route('/admin')
-def admin_panel():
-    from models import Users
-    users = Users.query.all()
-    return render_template('pages/admin_panel.html', users=users)
-
-
-@app.route('/delete_user/<int:user_id>', methods=['POST'])
-def delete_user(user_id):
-    from models import Users, User_details
-
-    user = Users.query.get(user_id)
-    if user:
-
-        user_details = User_details.query.filter_by(user_id=user_id).all()
-        for detail in user_details:
-            db.session.delete(detail)
-
-        db.session.delete(user)
-        db.session.commit()
-
-    return redirect(url_for('admin_panel'))
-
 
 if __name__ == '__main__':
     db.create_all()
